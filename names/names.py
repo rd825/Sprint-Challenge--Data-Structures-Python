f830562f0345ecaba8ea8672224efdbc0c16ecaf
Rahul Desai <rd825@georgetown.edu>
2019-03-01 12:57:13 -0500

    completed stretch for names

--- names/names.py
+++ names/names.py
@@ -10,16 +10,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# initial solution
 duplicates = set(names_1).intersection(names_2)
 
+# stretch solution
+# has better space complexity but is far slower than above
+# duplicates = [item for item in names_1 if item in names_2]
 
 end_time = time.time()
 # note to self -- expect 64 dupes
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
 
-
 # PROVIDED CODE
+# duplicates = []
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
